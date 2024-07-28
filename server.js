7508419b15fbbf2722673c544b1ba53a121262b6
David Nahum <david.n@top-group.co.il>
2024-07-28 14:00:50 +0300

    Add bootcamps routes

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const http = require('http');
+const morgan = require('morgan');
+//ROUTES
+const bootcamps = require('./routes/bootcamps');
 
 //LOAD ENV VARIABLES
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 const app = express();
-const PORT = process.env.PORT || 5000;
 
+
+//DEV LOGGING MIDLEWARE
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+//MOUNT ROUTERS
+app.use('/api/v1/bootcamps', bootcamps);
+
+
+const PORT = process.env.PORT || 5000;
+//SERVER UP
 app.listen(PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
