bb442fbd9734565df0995d34f70a29d863a7bb45
David Nahum <david.n@top-group.co.il>
2024-07-31 17:36:36 +0300

    Added colors to console

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const http = require('http');
 const morgan = require('morgan');
+const colors = require('colors');
 const connectDB = require('./config/db');
 
 //Load env variables
@@ -27,13 +28,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold)
 );
 
 
 //Handle unhandled promise regections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.bold);
     //Close server & exit process
     server.close(() => {
         process.exit(1);
